827b83f2a078feabb04ec6ae1074008d1fb0e14c
jvtesteves <joaovtesteves2002@gmail.com>
2024-05-21 15:54:30 -0300

    First commit

---
+++ src/main/java/com/jvtesteves/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package com.jvtesteves;
+
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
---
+++ src/main/java/com/jvtesteves/NavegadorInternet.java
@@ -0,0 +1,7 @@
+package com.jvtesteves;
+
+public interface NavegadorInternet {
+    void exibirPagina(String url);
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
